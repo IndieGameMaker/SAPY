738bd8e8b21848218d2b16ea40da53d5a867c8c1
Lee JaeHyun <myleje@gmail.com>
2021-06-12 09:03:26 +0900

    Upload Project

---
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    // 가장 먼저 호출되는 함수, 1번만 호출
+    void Awake()
+    {
+
+    }
+
+    // 활성화 될때 마다 호출
+    void OnEnable()
+    {
+
+    }
+
+    // 1번 호출 , 제일 먼저 호출
+    void Start()
+    {
+
+    }
+
+    // 랜더링 주기
+    void Update()
+    {
+
+    }
+
+    // 물리엔진 계산 주기 0.03 sec
+    void FixedUpdate()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+
+    }
+}
